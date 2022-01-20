b9c22e91c893d91b7f5b57e96e8b708e5723c141
ibidyouadu <angel.d.umana@gmail.com>
2022-01-19 23:55:08 -0500

    add tranlations for reset filters button

--- R/mod_main.R
+++ R/mod_main.R
@@ -24,32 +24,29 @@ mainUI <- function(id) {
                      status = 'success',
                      style='material-circle'
             ),
-            div(id="reset-filters", actionButton(ns('resetFilters'), "Reset filters")),
+            uiOutput(ns("resetFiltersUI")),
             div(style='flex-grow: 1;'),
             div(id='help-button', icon('question-circle-o'), onclick='tour()'),
             div(id="lang-select", selectInput(ns("language"), "", width = 80,
-                  c("EN" = "English",
-                    "ID" = "Bahasa Indonesia",
-                    "BR" = "Português (BR)",
-                    "MZ" = "Português (MZ)",
-                    "ES" = "Español",
-                    "PH" = "Philippines"
-                    ))
-                ),
+                                              c("EN" = "English",
+                                                "ID" = "Bahasa Indonesia",
+                                                "BR" = "Portugues (BR)",
+                                                # "MZ" = "Portugues (MZ)",
+                                                "ES" = "Español",
+                                                "PH" = "Philippines"
+                                              ))
+            ),
             div(class = 'fs-button',
                 HTML(
                   "<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='currentColor' class='bi bi-fullscreen' viewBox='0 0 16 16'>
-            <path d='M1.5 1a.5.5 0 0 0-.5.5v4a.5.5 0 0 1-1 0v-4A1.5 1.5 0 0 1 1.5 0h4a.5.5 0 0 1 0 1h-4zM10 .5a.5.5 0 0 1 .5-.5h4A1.5 1.5 0 0 1 16 1.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 0-.5-.5h-4a.5.5 0 0 1-.5-.5zM.5 10a.5.5 0 0 1 .5.5v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 0 14.5v-4a.5.5 0 0 1 .5-.5zm15 0a.5.5 0 0 1 .5.5v4a1.5 1.5 0 0 1-1.5 1.5h-4a.5.5 0 0 1 0-1h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 1 .5-.5z'/>
-          </svg>"),
+                    <path d='M1.5 1a.5.5 0 0 0-.5.5v4a.5.5 0 0 1-1 0v-4A1.5 1.5 0 0 1 1.5 0h4a.5.5 0 0 1 0 1h-4zM10 .5a.5.5 0 0 1 .5-.5h4A1.5 1.5 0 0 1 16 1.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 0-.5-.5h-4a.5.5 0 0 1-.5-.5zM.5 10a.5.5 0 0 1 .5.5v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 0 14.5v-4a.5.5 0 0 1 .5-.5zm15 0a.5.5 0 0 1 .5.5v4a1.5 1.5 0 0 1-1.5 1.5h-4a.5.5 0 0 1 0-1h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 1 .5-.5z'/></svg>"
+                ),
                 onclick = "shinyjs.toggleFullScreen();"
             )
-        ),
+        ), # flow-div
         uiOutput(ns("tabPanels"))
-      )
-    )
-  # tagList(
-  #   uiOutput(ns("main"))
-  # )
+      ) # tagList
+  ) # fillPage
 }
 
 #' main Server Function
@@ -60,6 +57,10 @@ mainServer <- function(id, state) {
   moduleServer(
     id,
     function(input, output, session) {
+      output$resetFiltersUI <- renderUI({
+        div(id="reset-filters", actionButton(ns('resetFilters'), tr(state, "Reset filters")))
+      })
+      
       output$tabPanels <- renderUI({
         ui <- list(
           # mainPanel(
